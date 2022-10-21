01ca555afdd1096da38dfc29033c95569394a88b
Luke Gartner <lukegartner@SH-Luke-G.local>
2022-10-21 11:22:14 -0500

    initial-commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quidem
+        voluptatum, perferendis ab libero, itaque, aliquam ex ratione ea maiores
+        corrupti eos voluptas! Asperiores perferendis, repellendus iste illum
+        porro commodi. Ex in consequuntur commodi quidem! Doloribus repudiandae
+        commodi numquam dolor?
+      </p>
+    </section>
+  );
+};
+
+export default About;
